6239894247e3d119dc3fad46f6e81af174a38789
ayoadepitan <54920436+ayoadepitan@users.noreply.github.com>
2022-01-12 10:17:27 -0600

    added ability to generate random password

--- addpass.py
+++ addpass.py
@@ -1,6 +1,8 @@
 import getpass
 import re
 import csv
+import random
+import string
 
 
 def check_website(website):
@@ -24,25 +26,52 @@ def check_email(email):
     return re.fullmatch(regex, email)
 
 
+def password_generator(size):
+    # Generates random string according to the size inputted
+    return ''.join(random.choices(string.ascii_letters + string.digits + '!@?#$&', k=size))
+
+
 def get_info():
+    # Checks if input is valid website
     while True:
         website = input('Website: ')
         if check_website(website):
             break
         print('Not a valid website. Please try again')
 
+    # Checks if input is valid email
     while True:
         email = input('Email: ')
         if check_email(email):
             break
         print('Not a valid email. Please try again')
 
-    password = getpass.getpass()
+    # Asks if you want to generate a password
+    # If yes then it runs password_generator() and returns random string
+    # If no then it asks for you to enter your own password that's hidden.
+    while True:
+        type = input('Do you want to generate a random password? (y/n): ')
+        if type in {'y', 'Y', 'yes', 'Yes'}:
+            while True:
+                try:
+                    length = int(
+                        input('Password length: '))
+                    break
+                except ValueError:
+                    print('Oops! That wasn\'t a valid number. Try again...')
+            password = password_generator(length)
+            break
+        elif type in {'n', 'N', 'no', 'No'}:
+            password = getpass.getpass()
+            break
+        else:
+            print('Not a valid answer. Try again...')
 
     write_to_csv(website, email, password)
 
 
 def write_to_csv(website, email, password):
+    # Writes do database.csv
     with open('database.csv', mode='a', newline='') as database:
         csv_writer = csv.writer(database)
         csv_writer.writerow([website, email, password])
